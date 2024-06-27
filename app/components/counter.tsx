06a6eed0bf9ea128a8d218f555af6338ddad5b1f
jbazanaguirre <jbazanaguirre@gmail.com>
2024-06-27 17:11:10 -0300

    add more garbage

--- app/components/counter.tsx
+++ app/components/counter.tsx
@@ -1,11 +1,13 @@
-import { ReactElement, useState } from "react";
+import SomeServerComponent from "./server-only";
 
-const Counter = (props: { someSubComponent?: ReactElement }) => {
+
+import { useState } from "react";
+
+const Counter = () => {
     const [count, setCount] = useState(0);
     return (
         <div className="p-4 border rounded-lg shadow-lg text-center space-y-4">
-            {/* <SomeServerComponent /> */}
-            {props.someSubComponent}
+            <SomeServerComponent pathname={"some-file.txt"} />
             <button 
             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
             onClick={() => setCount(count + 1)}>Count: {count}</button>
@@ -14,6 +16,3 @@ const Counter = (props: { someSubComponent?: ReactElement }) => {
     }
 
 export default Counter;
-
-
-
--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -4,7 +4,7 @@ import SomeServerComponent from '../components/server-only';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Counter someSubComponent={<SomeServerComponent pathname={"some-file.txt"}/>}/>
+      <Counter/>
     </main>
   );
 }
